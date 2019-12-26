33a9e5f35567eeb68be2b97399a6bf9bbf45a162
yarallira555 <yarallira555@outlook.com.br>
2019-12-26 16:20:35 -0300

    Melhorias no aplicativo de notas

--- AppNotas/AppNotas/AppNotas/obj/Debug/netstandard2.0/Views/PageHome.xaml.g.cs
+++ AppNotas/AppNotas/AppNotas/obj/Debug/netstandard2.0/Views/PageHome.xaml.g.cs
@@ -17,12 +17,8 @@ namespace AppNotas.Views {
     public partial class PageHome : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
-        private global::Xamarin.Forms.Label Desenvolvedora;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(PageHome));
-            Desenvolvedora = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "Desenvolvedora");
         }
     }
 }
